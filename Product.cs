599747f756d62c7eef88b877d15b4ae5b02868f5
Geres Maria Gutierrez <geresmariapgutierrez@iskolarngbayan.pup.edu.ph>
2026-04-12 06:45:29 +0800

    Tried parsing so far... commited some erroes

--- Product.cs
+++ Product.cs
@@ -5,7 +5,7 @@ class Product
     public int Id;
     public string Name;
     public double Price;
-    public int RemainingStock;
+    public int RemainingStock;///
 
     // Display product info
     public void DisplayProduct()
@@ -21,9 +21,82 @@ class Product
     }
 }
 
+
+
+
+
+
 class CartItem
 {
     public Product Product;
     public int Quantity;
     public double SubTotal;
 }
+
+class Program
+{
+    static void Main()
+    {
+        // Step 1: Create products array
+        Product[] Store = new Product[5];
+
+        Store[0] = new Product { Id = 1, Name = "Boots", Price = 3000, RemainingStock = 5 };
+        Store[1] = new Product { Id = 2, Name = "Loafers ", Price = 2500, RemainingStock = 10 };
+        Store[2] = new Product { Id = 3, Name = "Rubber Shoes", Price = 1900, RemainingStock = 8 };
+        Store[3] = new Product { Id = 4, Name = "Kitten Heels", Price = 1400, RemainingStock = 3 };
+        Store[4] = new Product { Id = 5, Name = "Slides", Price = 1500, RemainingStock = 15 };
+
+
+        // Step 2: Create cart array
+        CartItem[] Cart = new CartItem[10];
+        int CartCount = 0;
+
+        string AddMore = "Y";
+
+        while (AddMore.ToUpper() == "Y")
+        {
+            Console.WriteLine("\n--- Store Menu ---");
+
+            for (int i = 0; i < Store.Length; i++)
+            {
+                Store[i].DisplayProduct();
+            }
+
+            // Step 3: Get product number
+            Console.Write("Enter product number: ");
+            string InputProduct = Console.ReadLine();
+            int ProductNumber;
+
+            if (!int.TryParse(InputProduct, out ProductNumber) || ProductNumber < 1 || ProductNumber > Store.Length)
+            {
+                Console.WriteLine("Invalid product number!");
+                continue;
+            }
+
+            Product SelectedProduct = Store[ProductNumber - 1];
+
+            if (SelectedProduct.RemainingStock == 0)
+            {
+                Console.WriteLine("Sorry, this product is out of stock!");
+                continue;
+            }
+
+            // Step 3: Get quantity
+            Console.Write("Enter quantity: ");
+            string InputQuantity = Console.ReadLine();
+            int Quantity;
+
+            if (!int.TryParse(InputQuantity, out Quantity) || Quantity < 1)
+            {
+                Console.WriteLine("Invalid quantity!");
+                continue;
+            }
+
+            if (Quantity > SelectedProduct.RemainingStock)
+            {
+                Console.WriteLine("Not enough stock available.");
+                continue;
+            }
+
+            // Step 6: Check for duplicate cart entries
+          
--- obj/Debug/net10.0/Gutierrez_GeresMaria_ShoppingCartActivity.AssemblyInfo.cs
+++ obj/Debug/net10.0/Gutierrez_GeresMaria_ShoppingCartActivity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gutierrez_GeresMaria_ShoppingCartActivity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23eefa424879868a7203ad4769b085c976a23e93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+722c2412a63eb9430a5c3c1e156222507b4b6fed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gutierrez_GeresMaria_ShoppingCartActivity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gutierrez_GeresMaria_ShoppingCartActivity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
